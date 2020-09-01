11eb5f0605542f3db2c7eb641218e9db406b015f
Frank Dong <frank.dong2906@gmail.com>
2020-09-01 16:50:15 +0100

    Change name of character_count function to player_list because it fits the purpose of the function better.

--- script.py
+++ script.py
@@ -1,12 +1,12 @@
 from random import randint
 
-# --- Creates a list of characters, depending on the amount of players entered --- #
-def character_count(count):
-    characters = []
-    for number in range(1, count + 1):
-        character_name = 'Character ' + str(number)
-        characters.append(character_name)
-    return characters
+# --- Creates a list of player, depending on the amount of players entered --- #
+def player_list(players):
+    players = []
+    for number in range(1, players + 1):
+        player_name = 'Player ' + str(number)
+        players.append(player_name)
+    return players
 
 # --- Allows you to pick from a list of revolvers, which will be used in the storyline. --- #
 def revolver_selection(revolver_name):
@@ -24,7 +24,7 @@ def storyline(time_in_day, place_in_game):
     lower_time_in_day = time_in_day.lower()
     lower_place_in_game = place_in_game.lower()
 
-    if len(list_of_characters) == 1:
+    if len(list_of_players) == 1:
         if lower_time_in_day == 'day' and lower_place_in_game == 'kitchen':
             try:
                 print('It is daytime. The sun is shining outside. It is a pleasant and warm day with a small, fresh breeze.\nYou are alone in the kitchen. You take out your ' + revolver_choice + '. You decided to play some Russian Roulette.')
@@ -48,7 +48,7 @@ def storyline(time_in_day, place_in_game):
         else:
             print('Something went wrong. The options available for time are \'day\' and \'night\' and the options available for place are \'kitchen\' and \'bar\'. Try again!')
             
-    elif len(list_of_characters) > 1:
+    elif len(list_of_players) > 1:
         if lower_time_in_day == 'day' and lower_place_in_game == 'kitchen':
             try:
                 print('It is daytime. The sun is shining outside. It is a pleasant and warm day with a small, fresh breeze.\nYou are with your friends in the kitchen. You take out your ' + revolver_choice + '. You decided to play some Russian Roulette with your friends.')
@@ -74,9 +74,11 @@ def storyline(time_in_day, place_in_game):
 
 
 
-list_of_characters = character_count(1)
+
+
+list_of_players = player_list(4)
 #print(list_of_characters)
-revolver_choice = revolver_selection('MP412')
+revolver_choice = revolver_selection('.44 Magnum')
 #print(revolver_choice)
 storyline('day', 'kitchen')
 
